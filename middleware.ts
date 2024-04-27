c5b92cf1a905c8bed7e69766f1d544bed5518231
Lucas Roig <lucas.roig1@gmail.com>
2024-04-27 20:08:08 +0200

    feat(dark-mode)

--- middleware.ts
+++ middleware.ts
@@ -3,6 +3,7 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 const isPublic = createRouteMatcher([
   '/sign-in(.*)',
   '/sign-up(.*)',
+  '/public-images(.*)'
 ]);
 
 
